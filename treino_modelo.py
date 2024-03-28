55dc48f4a50f8d81189ef8ca3664fbaba7343846
Thiago <thiagobarroso11@hotmail.com>
2024-03-28 18:06:03 -0300

    ajuses

--- treino_modelo.py
+++ treino_modelo.py
@@ -122,6 +122,8 @@ if __name__ == '__main__':
    os.system('cls')
 
    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+   print(device)
+
    modelo = Conv_pytorch((1, 28, 28), device)
    print(modelo)
 
