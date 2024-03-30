13770f8e13603f20d07bac1568c1bb7e8909a14f
Thiago <thiagobarroso11@hotmail.com>
2024-03-29 22:03:42 -0300

    ajustes do modelo e adição de novas funcionalidades

--- modelos.py
+++ modelos.py
@@ -2,19 +2,40 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from torch.utils.data import DataLoader
 
 class Conv_pytorch(nn.Module):
-   def __init__(self, entrada: tuple):
+   def __init__(self, entrada: tuple, device: torch.device):
+      """
+         Modelo convolucional usado para testes
+
+         entrada: tupla contendo o formato de entrada (prof, alt, larg)
+         device: dispositivo onde o modelo estará sendo usado (cpu ou gpu)
+      """
       super(Conv_pytorch, self).__init__()
+
+      self.device = device if device is not None else 'cpu'
+
       prof, alt, larg = entrada[0], entrada[1], entrada[0]
 
       self.conv1 = nn.Conv2d(in_channels=prof, out_channels=32, kernel_size=3)
-      self.conv2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3)
+      self.conv2 = nn.Conv2d(in_channels=self.conv1.out_channels, out_channels=32, kernel_size=3)
       self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
       self.fc1 = nn.Linear(32 * 5 * 5, 128)
-      self.fc2 = nn.Linear(128, 10)
+      self.fc2 = nn.Linear(self.fc1.out_features, 10)
+
+      self.otimizador = optim.Adam(self.parameters(), lr=0.001)
+      self.perda = nn.CrossEntropyLoss()
+      
+      self.to(self.device) # tentar usar na gpu para acelerar
 
    def forward(self, x):
+      """
+         Alimenta os dados de entrada através do modelo.
+         
+         x: dados de entrada.
+      """
+
       x = F.relu(self.conv1(x))
       x = self.pool(x)
       x = F.relu(self.conv2(x))
@@ -22,26 +43,69 @@ class Conv_pytorch(nn.Module):
       x = x.view(-1, 32 * 5 * 5)
       x = F.relu(self.fc1(x))
       x = self.fc2(x)
+
       return F.softmax(x, dim=1)
    
-   def train_model(self, train_loader, epochs=5,device="cpu"):
-      optimizer = optim.Adam(self.parameters(), lr=0.001)
-      criterion = nn.CrossEntropyLoss()
-      self.to(device=device)
+   def treinar(self, treino: DataLoader, epochs: int) -> list:
+      self.train() #modo treino
+      historico = []
+      print('Treinando')
+
       for epoch in range(epochs):
-         running_loss = 0.0
-         for i, data in enumerate(train_loader, 0):
-               inputs, labels = data
-               inputs, labels = inputs.to(device), labels.to(device)
-               optimizer.zero_grad()
+         print(f"Epoch {epoch+1}/{epochs}")
 
-               outputs = self.forward(inputs)
-               loss = criterion(outputs, labels)
-               loss.backward()
-               optimizer.step()
+         for i, data in enumerate(treino, 0):
+            # Get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data[0].to(self.device), data[1].to(self.device)
 
-               running_loss += loss.item()
-               if i % 100 == 99:  # Imprimir a cada 100 mini-lotes
-                  print('[%d, %5d] loss: %.3f' %
-                        (epoch + 1, i + 1, running_loss / 100))
-                  running_loss = 0.0
+            # Zero the parameter gradients
+            self.otimizador.zero_grad()
+
+            # Forward and backward pass
+            outputs = self(inputs)
+            loss = self.perda.forward(outputs, labels)
+            historico.append(loss.item())
+            loss.backward()
+
+            # Update weights
+            self.otimizador.step()
+
+      return historico
+
+   def avaliar(self, teste: DataLoader) -> dict:
+      """
+         Calula dados para métrica de desempenho do modelo.
+
+         teste: conjunto de dados de teste.
+
+         return: dicionário contendo o valor de perda e precisão do modelo. 
+      """
+
+      self.eval() # modo avaliação
+      val_perda = 0.0
+      acertos = 0
+      total = 0
+
+      with torch.no_grad():
+         for data in teste:
+            entradas, saidas = data[0].to(self.device), data[1].to(self.device)
+
+            outputs = self(entradas)
+            loss = self.perda(outputs, saidas)
+            val_perda += loss.item()
+            _, previsto = torch.max(outputs.data, 1)
+            total += saidas.size(0)
+            acertos += (previsto == saidas).sum().item()
+
+      res = {}
+      res['loss'] = val_perda / len(teste)
+      res['accuracy'] = 100 * (acertos / total)
+      return res
+   
+   def salvar(self, caminho: str):
+      """
+         Exporta o modelo num aquivo externo.
+      """
+
+      torch.save(obj=self.state_dict(), f=caminho)
+      print('Modelo salvo')
--- treino_modelo.py
+++ treino_modelo.py
@@ -1,116 +1,14 @@
-from keras.models import Sequential
-from keras.layers import (InputLayer, Dense, Conv2D, ConvLSTM2D,MaxPool2D, TimeDistributed, Dropout, Flatten)
-from keras.optimizers import SGD
-from keras.datasets import (mnist, cifar10)
-from keras.utils import to_categorical
-from keras.activations import leaky_relu
-import numpy as np
-import os
-import matplotlib.pyplot as plt
-import tensorflow as tf
-
 import torch
 from modelos import Conv_pytorch
 import torchvision
 import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+import numpy as np
+import os
+import matplotlib.pyplot as plt
 
-
-def criar_modelo_convolucional() -> Sequential:
-   """
-      Modelos com camadas convolucionais
-   """
-
-   modelo = Sequential([
-      InputLayer((28, 28, 1)),
-      Conv2D(filters=32, kernel_size=(3, 3), activation='relu'),
-      MaxPool2D((2, 2)),
-      Conv2D(filters=32, kernel_size=(3, 3), activation='relu'),
-      MaxPool2D((2, 2)),
-      Flatten(),
-      Dense(128, activation='relu'),
-      Dense(10, activation='softmax')
-   ])
-
-   modelo.compile(
-      "adam",
-      "categorical_crossentropy",
-      metrics=['accuracy']
-   )
-
-   return modelo
-
-def criar_modelo_convolucional_lstm() -> Sequential:
-   """
-      Modelos usando camadas convolucionais com memória
-   """
-
-   seq = Sequential([
-      InputLayer((None, 28, 28, 1)),
-      ConvLSTM2D(filters=32, kernel_size=(3, 3), activation='relu', return_sequences=True),
-      TimeDistributed(MaxPool2D((2, 2))),
-      ConvLSTM2D(filters=64, kernel_size=(3, 3), activation='relu'),
-      MaxPool2D((2, 2)),
-      Flatten(),
-      Dense(128, activation='relu'),
-      Dense(10, activation="softmax")
-   ])
-
-   seq.compile(
-      SGD(0.01, 0.9),
-      "categorical_crossentropy",
-      metrics=['accuracy']
-   )
-
-   return seq
-
-def criar_modelo_mlp() -> Sequential:
-   """
-      Modelos com apenas camadas densas
-   """
-
-   seq = Sequential([
-      InputLayer((28, 28, 1)),
-      Flatten(),
-      Dense(28, activation='sigmoid'),
-      Dense(28, activation='sigmoid'),
-      Dense(10, activation="softmax")
-   ])
-
-   seq.compile(
-      "adam",
-      "categorical_crossentropy",
-      metrics=['accuracy']
-   )
-
-   return seq
-
-def carregar_dados(n_treino: int=100, n_teste: int=100):
-   (treino_x, treino_y), (teste_x, teste_y) = mnist.load_data()
-
-   # Selecionar um subconjunto aleatório de dados
-   treino_x = treino_x[:n_treino]
-   treino_y = treino_y[:n_treino]
-   teste_x = teste_x[:n_teste]
-   teste_y = teste_y[:n_teste]
-
-   # Normalizar os valores de pixel para o intervalo [0, 1]
-   treino_x = treino_x.astype('float32') / 255.0
-   teste_x = teste_x.astype('float32') / 255.0
-
-   # Converter rótulos em formato one-hot
-   treino_y = to_categorical(treino_y, num_classes=10)
-   teste_y = to_categorical(teste_y, num_classes=10)
-   print(treino_x.shape)
-
-   return treino_x, treino_y, teste_x, teste_y
-
-def treinar_modelo(modelo: Sequential, treino_x, treino_y, epocas: int, verbose=0):
-   historico = modelo.fit(treino_x, treino_y, epochs=epocas, verbose=verbose)
-   return historico
-
-def plotar_historico(historico):
-   # Plotar a perda do modelo
-   plt.plot(historico.history['loss'], label='Perda de Treinamento')
+def plotar_historico(historico: list):
+   plt.plot(historico, label='Perda de Treinamento')
    plt.xlabel('Época')
    plt.ylabel('Perda')
    plt.title('Histórico de Perda do Modelo')
@@ -118,34 +16,46 @@ def plotar_historico(historico):
    plt.ylim(bottom=0)
    plt.show()
 
+def preparar_dataset() -> tuple[DataLoader]:
+   transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+
+   # Carregar os datasets de treinamento e teste
+   train_dataset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+   test_dataset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+
+   # Criar DataLoaders para carregamento em lote
+   train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True)
+   test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=64, shuffle=False)
+
+   return (train_loader, test_loader)
+
 if __name__ == '__main__':
    os.system('cls')
 
    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-   modelo = Conv_pytorch((1, 28, 28)).to(device)
+   modelo = Conv_pytorch(entrada=(1, 28, 28), device=device)
    print(modelo)
+   print(f"Modelo em: {modelo.device}")
 
-   transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
-   trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
-   train_loader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+   transform = transforms.Compose([
+      transforms.ToTensor(), 
+      transforms.Normalize((0.5,), (0.5,))
+   ])
 
-   modelo.train_model(train_loader, 5,device)
+   mnist_treino = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+   ds_treino = DataLoader(mnist_treino, batch_size=64, shuffle=True)
 
-   exit()
+   mnist_teste = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+   ds_teste = DataLoader(mnist_teste, batch_size=64, shuffle=True)
 
-   epochs = 5
+   hist = modelo.treinar(ds_treino, 2)
 
-   treino_x, treino_y, teste_x, teste_y = carregar_dados(50_000, 10_000)
-   
-   # conv
-   conv = criar_modelo_convolucional()
-   historico_conv = treinar_modelo(conv, treino_x, treino_y, epochs, 1)
-   
-   perda, precisao  = conv.evaluate(treino_x, treino_y)
-   print("(Treino) -> perda: ", perda, " precisão: ", precisao)
-   
-   perda, precisao  = conv.evaluate(teste_x, teste_y)
-   print("(Teste) -> perda: ", perda, " precisão: ", precisao)
+   metricas_treino = modelo.avaliar(ds_treino)
+   metricas_teste = modelo.avaliar(ds_teste)
 
-   plotar_historico(historico_conv)
-   conv.save('modelos/keras/modelo-mnist.keras')
+   print('treino:', metricas_treino)
+   print('teste:', metricas_teste)
+
+   plotar_historico(hist)
+
+   modelo.salvar("./modelos/pytorch/conv-pytorch-mnist.pt")
