6fe6dd14a640f09968af6dd2833cb62a2bbe4b2b
Thiago <thiagobarroso11@hotmail.com>
2024-05-12 18:51:31 -0300

    testes

--- TreinoModelo.java
+++ TreinoModelo.java
@@ -28,8 +28,8 @@ public class TreinoModelo{
 	static final int NUM_DIGITOS_TESTE  = NUM_DIGITOS_TREINO;
 	static final int NUM_AMOSTRAS_TREINO = 400;//max 400
 	static final int NUM_AMOSTRAS_TESTE  = 100;//max 100
-	static final int TREINO_EPOCAS = 12;
-	static final int TREINO_LOTE = 10;
+	static final int TREINO_EPOCAS = 10;
+	static final int TREINO_LOTE = 8;
 	static final boolean TREINO_LOGS = true;
 
 	// caminhos de arquivos externos
@@ -44,7 +44,7 @@ public class TreinoModelo{
 		final var treinoX = utils.array4DParaTensors(carregarDadosMNIST(CAMINHO_TREINO, NUM_AMOSTRAS_TREINO, NUM_DIGITOS_TREINO));
 		final var treinoY = utils.array2DParaTensors(criarRotulosMNIST(NUM_AMOSTRAS_TREINO, NUM_DIGITOS_TREINO));
 
-		Sequencial modelo = modeloMlp();
+		Sequencial modelo = modeloConv();
 		modelo.setHistorico(true);
 		modelo.print();
 
@@ -79,10 +79,10 @@ public class TreinoModelo{
 			new Entrada(28, 28),
 			new Convolucional(new int[]{3, 3}, 16, "relu"),
 			new MaxPooling(new int[]{2, 2}),
-			new Convolucional(new int[]{3, 3}, 22, "relu"),
+			new Convolucional(new int[]{3, 3}, 20, "relu"),
 			new MaxPooling(new int[]{2, 2}),
 			new Flatten(),
-			new Densa(128, "tanh"),
+			new Densa(128, "sigmoid"),
 			new Densa(NUM_DIGITOS_TREINO, "softmax")
 		);
 
